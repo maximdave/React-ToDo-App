fb9a65bf76a419f2ca63ed21d56f0591dc3d01ae
David Enoragbon <david.enoragbon@decagon.dev>
2021-07-26 11:05:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo('');
+      return;
+    }
+
+    if (todo !== '') {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo('');
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>Todo List App</h1>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          todo={todo}
+          editId={editId}
+          setTodo={setTodo}
+        />
+
+        {todos.length>0 ? (
+          <TodoList
+            todos={todos}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        ) : (
+          <h3 style={{ margin: '90px auto' }}>Please Enter a Todo Item</h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
